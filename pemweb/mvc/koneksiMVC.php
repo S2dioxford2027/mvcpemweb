ffdc00e2c4dfb8c90d5ffeb1156f2a18310f842a
afifalhauzan <afifalhauzan2@gmail.com>
2024-12-01 10:23:15 +0700

    kodenya dibikin error

--- pemweb/mvc/koneksiMVC.php
+++ pemweb/mvc/koneksiMVC.php
@@ -1,5 +1,5 @@
 <?php
-$mysqli = new mysqli('localhost', 'root', '', 'localhost2');
+$mysqli = new mysqli('root', 'root', '', 'localhost2');
 
 if ($mysqli->connect_error) {
     die("Koneksi gagal: " . $mysqli->connect_error);
